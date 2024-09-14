4595c923141b6e63e2df58cbb5b2991169f83d59
root <root@MattsLaptop>
2024-09-14 11:05:05 +1000

    Implement delete parking spot function

--- functions/carpark_functions.py
+++ functions/carpark_functions.py
@@ -1,7 +1,6 @@
 from classes.parking_slot import ParkingSlot
 
 def add_slot(carpark):
-    print ("Adding slot")
     # Take input the id for the parking slot
     slot_id = input("Enter id for the parking slot: ")
     # Create an instance of the parking slot
@@ -9,6 +8,15 @@ def add_slot(carpark):
     # Add the slot to the carpark
     carpark.add_slot(parking_slot)
     print("Parking slot added\n")
+
+def delete_slot(carpark):
+    # Take input of id from the user
+    slot_id = input("Enter the id of the parking slot: ")
+    # Delete the parking slot
+    if carpark.delete_slot(slot_id):
+        print("Parking slot deleted\n")
+    else:
+        print("Parking slot not found.\n")
 
 def list_slots(carpark):
     all_slots = carpark.get_slots()
